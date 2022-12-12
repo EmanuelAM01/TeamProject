c02ef1653c8b55aca124763cc73ea6dfa2e48042
Andres felipe estrada cortes <afecestra@hotmail.com>
2022-12-12 09:32:49 -0500

    componente company

---
+++ client/src/componentes/Compani.js
@@ -0,0 +1,33 @@
+
+
+
+const Compani = () =>{
+    
+    return(
+        <div className="mainContainer" >
+            <br/>
+            <h1 className="container" ><b>Tu publicidad Online</b></h1>
+            <div className="container1">
+
+                <div className="container2"></div>
+                <div className="container3">In order to provide creative and high quality solutions to companies and independent entrepreneurs, born<br/> <b>Tu publicidad Online.</b></div>
+
+            </div>
+            
+            <div className="container5">A company focused on the elaboration of high quality advertising and lithographic products where we focus on contributing to the growth of our clients through products and strategies that allow us to offer optimal solutions to all the needs they may have. </div>
+            <br/>
+            <div className="container1">
+                <div className="container8">
+                Our team of people has extensive experience, trajectory, commitment and training, which keeps us as an innovative company that is always at the forefront of products and technologies that allow us to provide our clients with a final product according to their requirements and needs.
+                </div>
+            
+                <div className="container4"></div>  
+            </div>
+
+            <br/>
+            <p></p>
+        </div>
+    )
+}
+
+export default Compani;
